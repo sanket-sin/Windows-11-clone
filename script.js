f2ebd5f96dfabddc6cd6492696e471d5d5ba4efc
Sanket Sinha <sinhasanket3@gmail.com>
2021-06-27 16:34:30 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
+
+function openWin() {
+    window.open("http://www.google.com/");
+  }
